3180467e9f6d12ebf7d9f99039da0f9f0585e0ea
Michael McClimon <michael@mcclimon.org>
2023-04-29 16:31:31 -0400

    Add real values and a bunch of binary ops
    This is Chapter 18, which probably should have been more than one
    commit, but oh well.

--- chunk.h
+++ chunk.h
@@ -6,6 +6,13 @@
 
 typedef enum {
   OP_CONSTANT,
+  OP_NIL,
+  OP_TRUE,
+  OP_FALSE,
+  OP_EQUAL,
+  OP_GREATER,
+  OP_LESS,
+  OP_NOT,
   OP_NEGATE,
   OP_ADD,
   OP_SUBTRACT,
--- vm.c
+++ vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "common.h"
@@ -8,18 +9,41 @@
 
 static void resetStack(VM *vm) { vm->stackTop = vm->stack; }
 
+static void runtimeError(VM *vm, const char *format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fputs("\n", stderr);
+
+  size_t instruction = vm->ip - vm->chunk->code - 1;
+  int line = getLine(vm->chunk, (int)instruction);
+  fprintf(stderr, "[line %d] in script\n", line);
+  resetStack(vm);
+}
+
 void initVM(VM *vm) { resetStack(vm); }
 
 void freeVM(VM *vm) {}
 
+static Value peek(VM *vm, int distance) { return vm->stackTop[-1 - distance]; }
+
+static bool isFalsy(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run(VM *vm) {
 #define READ_BYTE() (*vm->ip++)
 #define READ_CONSTANT() (vm->chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op)                                                          \
+#define BINARY_OP(valueType, op)                                               \
   do {                                                                         \
-    double b = pop(vm);                                                        \
-    double *ap = vm->stackTop - 1;                                             \
-    *ap = *ap op b;                                                            \
+    if (!IS_NUMBER(peek(vm, 0)) || !IS_NUMBER(peek(vm, 1))) {                  \
+      runtimeError(vm, "Operands must be numbers.");                           \
+      return INTERPRET_RUNTIME_ERROR;                                          \
+    }                                                                          \
+    double b = AS_NUMBER(pop(vm));                                             \
+    double a = AS_NUMBER(pop(vm));                                             \
+    push(vm, valueType(a op b));                                               \
   } while (false)
 
   while (true) {
@@ -41,20 +65,48 @@ static InterpretResult run(VM *vm) {
       push(vm, constant);
       break;
     }
+    case OP_NIL:
+      push(vm, NIL_VAL);
+      break;
+    case OP_TRUE:
+      push(vm, BOOL_VAL(true));
+      break;
+    case OP_FALSE:
+      push(vm, BOOL_VAL(false));
+      break;
+    case OP_EQUAL: {
+      Value b = pop(vm);
+      Value a = pop(vm);
+      push(vm, BOOL_VAL(valuesEqual(a, b)));
+      break;
+    }
+    case OP_GREATER:
+      BINARY_OP(BOOL_VAL, >);
+      break;
+    case OP_LESS:
+      BINARY_OP(BOOL_VAL, <);
+      break;
     case OP_ADD:
-      BINARY_OP(+);
+      BINARY_OP(NUMBER_VAL, +);
       break;
     case OP_SUBTRACT:
-      BINARY_OP(-);
+      BINARY_OP(NUMBER_VAL, -);
       break;
     case OP_MULTIPLY:
-      BINARY_OP(*);
+      BINARY_OP(NUMBER_VAL, *);
       break;
     case OP_DIVIDE:
-      BINARY_OP(/);
+      BINARY_OP(NUMBER_VAL, /);
       break;
     case OP_NEGATE:
-      *(vm->stackTop - 1) *= -1;
+      if (!IS_NUMBER(peek(vm, 0))) {
+        runtimeError(vm, "Operand must be a number.");
+        return INTERPRET_RUNTIME_ERROR;
+      }
+      push(vm, NUMBER_VAL(-AS_NUMBER(pop(vm))));
+      break;
+    case OP_NOT:
+      push(vm, BOOL_VAL(isFalsy(pop(vm))));
       break;
     case OP_RETURN: {
       printValue(pop(vm));
